c72c02b337c1fba1db1d7a3d3a63cb794d915dad
helloye <helloye@localhost.localdomain>
2018-03-09 13:32:50 -0500

    Added csv parser. TODO: Run it through NLTK??

--- src/script1.py
+++ src/script1.py
@@ -1,1 +1,14 @@
-print 'Hello World!'
+import csv
+
+print ('Hello World!')
+
+#Note, port over csvfile into same /src dir to simplicity reference.
+with open('un-general-debates-short.csv', 'r') as csvfile:
+    freader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in freader:
+        #row[0]:session
+        #row[1]:year
+        #row[2]:country
+        #row[3]:text
+        print(row[3])
+    
